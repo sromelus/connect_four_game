e31600f056cf3e74c8f06343631392eb277f876d
sromelus <46148909+sromelus@users.noreply.github.com>
2020-05-08 13:16:49 -0400

    create spaces on the board class based on rows and columns

--- src/js/Board.js
+++ src/js/Board.js
@@ -1,7 +1,26 @@
 class Board {
   constructor() {
-    this.row = 6;
-    this.column = 7;
-    this.spaces = [];
+    this.rows = 6;
+    this.columns = 7;
+    this.spaces = this.createSpaces();
+  }
+
+  /**
+ * Generates 2D array of spaces.
+ * @return  {Array}     An array of space objects
+ */
+
+  createSpaces(){
+    const spaces = [];
+    for(x = 0; x < this.columns; x++){
+      const column = [];
+
+      for(y = 0; y < this.rows; y++){
+        column.push(new Space(x, y));
+      }
+
+      spaces.push(column);
+    }
+    return spaces;
   }
 }
